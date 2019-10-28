3ea2901431ed4acd0917e2ab80e72930c2c66bd8
Bruno Warwzeniak <bruno_p_w@hotmail.com>
2019-10-28 17:57:54 +0000

    Code refactoring

--- src/main/java/eventLogAnalyzer/Event.java
+++ src/main/java/eventLogAnalyzer/Event.java
@@ -3,43 +3,52 @@ package eventLogAnalyzer;
 import java.sql.Timestamp;
 
 public class Event {
-	
+
 	private String id;
 	private State state;
 	private Timestamp timestamp;
 	private String type;
 	private String host;
-	
+
 	public Event() {
 	}
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public State getState() {
 		return state;
 	}
+
 	public void setState(State state) {
 		this.state = state;
 	}
+
 	public Timestamp getTimestamp() {
 		return timestamp;
 	}
+
 	public void setTimestamp(Timestamp timestamp) {
 		this.timestamp = timestamp;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getHost() {
 		return host;
 	}
+
 	public void setHost(String host) {
 		this.host = host;
 	}
--- src/main/java/eventLogAnalyzer/EventLogAnalyzer.java
+++ src/main/java/eventLogAnalyzer/EventLogAnalyzer.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,13 +22,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class EventLogAnalyzer {
 	
 	private Connection conn = null;
-	private String db = "jdbc:hsqldb:hsql://localhost/sdb;ifexists=true";
+	private String db = "jdbc:hsqldb:hsql://localhost/edb;ifexists=true";
 	private String user = "SA";
 	private String password = "";
 	private List<Event> sortedEventsList = null;
 	
 	private Connection getConnection() {
-		// Create database connection
     	try {
     		Connection conn = DriverManager.getConnection(db, user, password);
     		conn.setAutoCommit(true);
@@ -40,7 +40,6 @@ public class EventLogAnalyzer {
 	
 	private void closeConnection(Connection conn) {
 		try {
-            // Close connection
             if (conn != null) 
                 conn.close();
         }
@@ -50,54 +49,53 @@ public class EventLogAnalyzer {
 	}
 	
 	private void insertOrUpdateEvent(Event event) {
-		/*if (sortedEventsList.get(i).getId().equals(sortedEventsList.get(i + 1).getId())) {
-			if ((sortedEventsList.get(i + 1).getTimestamp().getTime() - sortedEventsList.get(i).getTimestamp().getTime()) > 4) {
-				System.out.println(sortedEventsList.get(i).getId() + " - " + (sortedEventsList.get(i + 1).getTimestamp().getTime() - sortedEventsList.get(i).getTimestamp().getTime()));
-				i++;
-
-				ela.selectEventTable();
-			}
-		}*/		
         try {
         	Connection conn = getConnection();
-        	
-    		if (selectEventByIdUsingConnection(conn, event.getId())) {
-    			
-    			List<Event> filteredEvents = sortedEventsList.stream()
-                .filter(ev -> ev.getId().equals(event.getId()))
-                .collect(Collectors.toList());
-    			
-    			long duration = 0;
-    			if(filteredEvents.size() == 2) {
-    				duration = Math.abs(filteredEvents.get(0).getTimestamp().getTime() - filteredEvents.get(1).getTimestamp().getTime());
-    			}
-    			
-    			String sql = "UPDATE Event SET duration = ?, " +
-    										  "alert = ?     " +
-					    	 " WHERE id = ?";
-				
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setTimestamp(1, new Timestamp(duration));
-				pstmt.setBoolean(2, duration > 4 ? true : false);
-				pstmt.setString(3, event.getId());
-
-				pstmt.executeUpdate();
-	
-				pstmt.close();	         
-    		} else {
-	        	String sql = "INSERT INTO Event (id, type, host, alert) " +
-					    " VALUES (?,?,?,?)";
-				
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, event.getId());
-				pstmt.setString(2, event.getType());
-				pstmt.setString(3, event.getHost());
-				pstmt.setBoolean(4, false);
-				
-				pstmt.executeUpdate();
-	
-				pstmt.close();	         
-	        }
+    		
+        	synchronized (EventLogAnalyzer.class) {
+	    		if (selectEventByIdUsingConnection(conn, event.getId())) {
+	    			if (event.getState().equals(State.FINISHED) && durationNotUpdatedByIdUsingConnection(conn, event.getId()) ) {
+		    			List<Event> filteredEvents = sortedEventsList.stream()
+		                .filter(ev -> ev.getId().equals(event.getId()))
+		                .collect(Collectors.toList());
+		    			
+		    			// Remove the duplicates events
+		    			HashSet<Object> seen = new HashSet<>();
+		    			filteredEvents.removeIf(e -> ! seen.add(e.getId() + e.getState().name()));
+		    			
+		    			long duration = 0;
+		    			if(filteredEvents.size() > 1) {
+		    				duration = Math.abs(filteredEvents.get(0).getTimestamp().getTime() - filteredEvents.get(1).getTimestamp().getTime());
+		    			}
+		    			
+		    			String sql = "UPDATE Event SET duration = ?, " +
+		    										  "alert = ?     " +
+							    	 " WHERE id = ?";
+						
+						PreparedStatement pstmt = conn.prepareStatement(sql);
+						pstmt.setTimestamp(1, new Timestamp(duration));
+						pstmt.setBoolean(2, duration > 4 ? true : false);
+						pstmt.setString(3, event.getId());
+		
+						pstmt.executeUpdate();
+			
+						pstmt.close();	   
+	    			}
+	    		} else {
+		        	String sql = "INSERT INTO Event (id, type, host, alert) " +
+						    " VALUES (?,?,?,?)";
+					
+					PreparedStatement pstmt = conn.prepareStatement(sql);
+					pstmt.setString(1, event.getId());
+					pstmt.setString(2, event.getType());
+					pstmt.setString(3, event.getHost());
+					pstmt.setBoolean(4, false);
+					
+					pstmt.executeUpdate();
+		
+					pstmt.close();	         
+		        }
+    		}
         }
         catch (SQLException e) {
             System.err.println(e.getMessage());
@@ -119,8 +117,30 @@ public class EventLogAnalyzer {
             if(rs.next()) {
             	exist = true;
             }
+            
+            rs.close();
+            pstmt.close();
+            
+        }
+        catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } 
+        return exist;
+	}
+	
+	private boolean durationNotUpdatedByIdUsingConnection(Connection conn, String id) {
+		boolean exist = false;
+		try {
+			String sql = "select * from EVENT where id = ? and duration is null";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			
+            ResultSet rs =  pstmt.executeQuery();
              
-            // Clean up
+            if(rs.next()) {
+            	exist = true;
+            }
+            
             rs.close();
             pstmt.close();
             
@@ -134,11 +154,11 @@ public class EventLogAnalyzer {
 	private void selectEventTable() {
 		try {
         	Connection conn = getConnection();
-        	// Create and execute statement
-            Statement stmt = conn.createStatement();
+
+        	Statement stmt = conn.createStatement();
             ResultSet rs =  stmt.executeQuery("select * from EVENT");
              
-            // Loop through the data and print all artist names
+            System.out.println("----------");
             while(rs.next()) {
                 System.out.println("Event: " + rs.getString("ID") + 
                 		" " + rs.getTimestamp("DURATION") + 
@@ -146,8 +166,36 @@ public class EventLogAnalyzer {
                 		" " + rs.getString("HOST") +
                 		" " + rs.getString("ALERT"));
             }
+            System.out.println("----------");
+            
+            rs.close();
+            stmt.close();
+        }
+        catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+        	closeConnection(conn);
+        }
+	}
+	
+	private void selectAlertEvents() {
+		try {
+        	Connection conn = getConnection();
+
+        	Statement stmt = conn.createStatement();
+            ResultSet rs =  stmt.executeQuery("select * from EVENT where ALERT = TRUE");
              
-            // Clean up
+            System.out.println("------More than 4ms Events------");
+            while(rs.next()) {
+                System.out.println(rs.getString("ID") + 
+                		" " + rs.getTimestamp("DURATION") + 
+                		" " + rs.getString("TYPE") + 
+                		" " + rs.getString("HOST") + 
+                		" " + rs.getString("ALERT")); 
+            }
+            System.out.println("--------------------------------");
+            
             rs.close();
             stmt.close();
         }
@@ -162,11 +210,10 @@ public class EventLogAnalyzer {
 	private void dropTable() {
 		try {
         	Connection conn = getConnection();
-			// Create and execute statement
-	        Statement stmt = conn.createStatement();
+
+        	Statement stmt = conn.createStatement();
 	        stmt.execute("DROP TABLE event");
 	        
-	        // Clean up
 	        stmt.close();
         }
         catch (SQLException e) {
@@ -180,11 +227,10 @@ public class EventLogAnalyzer {
 	private void deleteTable() {
 		try {
         	Connection conn = getConnection();
-			// Create and execute statement
-	        Statement stmt = conn.createStatement();
+
+        	Statement stmt = conn.createStatement();
 	        stmt.execute("DELETE FROM event");
 	        
-	        // Clean up
 	        stmt.close();
         }
         catch (SQLException e) {
@@ -201,8 +247,8 @@ public class EventLogAnalyzer {
 		dropTable();
         try {
         	Connection conn = getConnection();
-			// Create and execute statement
-	        Statement stmt = conn.createStatement();
+
+        	Statement stmt = conn.createStatement();
 	        stmt.execute("CREATE TABLE IF NOT EXISTS event ( " + 
 						"id VARCHAR(255)," + 
 						"duration TIMESTAMP," + 
@@ -211,7 +257,6 @@ public class EventLogAnalyzer {
 						"alert BOOLEAN" + 
 						" )");
 	        
-	        // Clean up
 	        stmt.close();
         }
         catch (SQLException e) {
@@ -223,7 +268,8 @@ public class EventLogAnalyzer {
 	}
 
 	public static void main(String[] args)
-			throws JsonGenerationException, JsonMappingException, IOException, SQLException, ClassNotFoundException {
+			throws JsonGenerationException, JsonMappingException, IOException, SQLException, 
+			ClassNotFoundException {
 		
 		EventLogAnalyzer ela = new EventLogAnalyzer();
 		ela.populateSortedEventsList();
@@ -233,9 +279,9 @@ public class EventLogAnalyzer {
         
 		for (int i = 0; i < ela.sortedEventsList.size(); i++) {
 			ela.insertOrUpdateEvent(ela.sortedEventsList.get(i));
-			ela.selectEventTable();
 		}
-		ela.selectEventTable();
+		//ela.selectEventTable();
+		ela.selectAlertEvents();
 	}
 
 	private void populateSortedEventsList() throws IOException {
@@ -247,6 +293,6 @@ public class EventLogAnalyzer {
 				.sorted(Comparator.comparing(Event::getId)).collect(Collectors.toList());
 
 		eventMap.readAll().stream().sorted(Comparator.comparing(Event::getState))
-		.sorted(Comparator.comparing(Event::getId)).collect(Collectors.toList());
+				.sorted(Comparator.comparing(Event::getId)).collect(Collectors.toList());
 	}
 }
